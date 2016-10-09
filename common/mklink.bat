ace27b2ec87b2cc529373ad984d8bba5d1e90652
fly <fly@load-page.com>
2016-10-09 11:42:33 +0800

    upgrade

--- common/mklink.bat
+++ common/mklink.bat
@@ -33,12 +33,12 @@ if not "%1"=="" (
 	if exist "%CD%\..\%1" (
 		echo 重建%CD%\..\%1的公共DLL库文件指向
 		for /f "tokens=3,4 delims= " %%c in ('dir "%CD%\..\%1" ^| FIND "<SYMLINK"') do (
-			if "%%c" == "<SYMLINKD>" (rmdir %CD%\..\%1\%%d) else (del %CD%\..\%1\%%d)
+			if "%%c" == "<SYMLINKD>" (rmdir "%CD%\..\%1\%%d") else (del "%CD%\..\%1\%%d")
 		)
 		rem for /f "delims=" %%a in ('dir   /s /b /a-d^|findstr /v /i "mklink.bat"') do (echo %%a)
 		for /f "delims=" %%d in ('dir /b /on "%CD%\*.dll" "%CD%\*.pdb"') do (
 			rem	if exist %CD%\..\%%i\%%d (rmdir %CD%\..\%%i\%%d)
-			mklink %CD%\..\%1\%%d %CD%\%%d >nul
+			mklink "%CD%\..\%1\%%d" "%CD%\%%d" >nul
 		)
 		echo done!
 		echo.
